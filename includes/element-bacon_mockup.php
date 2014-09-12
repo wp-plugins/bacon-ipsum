04294ff6b11ca60b161f8db45000f24fe97c9382
fristopher <fristopher@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-12 15:08:05 +0000

    fixed default tag issue
    git-svn-id: https://plugins.svn.wordpress.org/bacon-ipsum/trunk@988343 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/element-bacon_mockup.php
+++ includes/element-bacon_mockup.php
@@ -1,10 +1,7 @@
 <?php
 
 
-//$bacon_tag = $atts['tag'] ;
-
-$bacon_tag = ($atts['tag'] == "none") ? '' : $atts['tag'];
-
+$bacon_tag = (strpos($atts['tag'],"||") || $atts['tag'] == "none") ? '' : $atts['tag'];
 ?>
 
 <img class="<?php echo implode(", ", (array)$atts['class']); ?>" width="<?php echo implode(", ", (array)$atts['width']); ?>" height="<?php echo implode(", ", (array)$atts['height']); ?>" src="http://baconmockup.com/<?php echo implode(", ", (array)$atts['width']); ?>/<?php echo implode(", ", (array)$atts['height']); ?>/<?php echo $bacon_tag;?>" alt="<?php echo implode(", ", (array)$atts['alt']); ?>">
